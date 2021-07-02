41e99d27d1905d3e44f53d3e7932882624db78aa
hasshild <86813620+hasshild@users.noreply.github.com>
2021-07-02 12:11:22 +0900

    Greetings added

--- Ex4.java
+++ Ex4.java
@@ -1,8 +1,16 @@
 //File:Ex4.java
 import java.util.Random;
+import java.util.Scanner;
 
 class Ex4{
 	public static void main(String[] args){
+		//名前を聞くパート
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = scan.next();
+		System.out.println("Hello, " +name +"!");
+		
+		//サイコロパート
 		Random rand = new Random();
 		int x1 = rand.nextInt(5) + 1;
 		int x2 = rand.nextInt(5) + 1;
