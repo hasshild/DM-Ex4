921aff400af93361666c26342db574ab28dd146b
hasshild <86813620+hasshild@users.noreply.github.com>
2021-07-02 12:43:49 +0900

    Won/lost message modified

--- Ex4.java
+++ Ex4.java
@@ -23,7 +23,7 @@ class Ex4{
 		System.out.println("Total value: " +total);
 		
 		//勝ち負けを判定するパート
-		if(total > 7) System.out.println("You won!");
-		else System.out.println("You lost!");
+		if(total > 7) System.out.println(name +" won!");
+		else System.out.println(name +" lost!");
 	}
 }
